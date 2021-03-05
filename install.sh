6095658a7e06dca8baf0769d116e8ad009779198
justcli <justcli@fastmail.com>
2021-03-06 00:36:07 +0530

    updated

--- install.sh
+++ install.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-ver=`/usr/bin/env python -c "from __future__ import print_function;import sys;print(sys.version[0])"`
-if [ $? -ne 0 ];then
-	echo "Could find any python installation."
-	exit 1
-fi
-pyexe=`which python`
-if [ $ver -ne 3 ]; then
-	which python3 1>/dev/null 2>&1
-	if [ $? -ne 0 ];then
-		echo "Could not find any python3 installation."
-		exit 1
-	fi
-	pyexe=`which python3`
-fi
-
 install_path="/usr/local/bin/"
 
 cp pydep $install_path
