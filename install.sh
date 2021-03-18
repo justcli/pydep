486cd110eb2be061fe9e63fcbffaf14e8614d85e
justcli <justcli@fastmail.com>
2021-03-19 00:04:28 +0530

    updated

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
 install_path="/usr/local/bin/"
 
-cp pydep $install_path
+cp -f pydep $install_path
 if [ $? -ne 0 ];then
 	echo "Unable to copy files to "$install_path\
 			 ". Try running the script as sudo e.g. > sudo ./install.sh"
